8d629f1c8e8f2623395b11862b42d9ef7d20592b
Renan Castro <renandf@gmail.com>
2020-09-12 09:20:17 +1000

    authentication hook with signIn/signOut

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 import Login from './pages/Login';
-import SignUp from './pages/SignUp';
+// import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
+
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <Login />
+
+    <AuthProvider>
+      <Login />
+    </AuthProvider>
   </>
 );
 
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -4,6 +4,7 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -13,10 +14,17 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const Login: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const { signIn } = useAuth();
+
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
       formRef.current?.setErrors({});
 
@@ -26,12 +34,17 @@ const Login: React.FC = () => {
       });
 
       await schema.validate(data, { abortEarly: false });
+
+      signIn({
+        email: data.email,
+        password: data.password
+      });
     } catch (err) {
       const errors = getValidationErrors(err);
 
       formRef.current?.setErrors(errors);
     }
-  }, []);
+  }, [signIn]);
 
   return (
     <Container>
@@ -50,7 +63,7 @@ const Login: React.FC = () => {
 
         </Form>
 
-        <a href="#">
+        <a href="/signup">
           <FiLogIn />
           Sign up
         </a>
