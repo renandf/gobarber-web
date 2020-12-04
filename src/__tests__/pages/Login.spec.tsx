efb37bb9f97df12e304ab7a09544800bce985c1c
Renan Castro <renandf@gmail.com>
2020-12-05 08:16:36 +1100

    updated @testing-libraries and "test" script so Jest could run properly; Login page test done

--- src/__tests__/pages/Login.spec.tsx
+++ src/__tests__/pages/Login.spec.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 
 import Login from '../../pages/Login';
 
+const mockedHistoryPush = jest.fn();
+
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
-describe('Login Page', () => {
-  it('should be able to log in', () => {
-    const { debug } = render(<Login />);
+jest.mock('../../hooks/auth', () => {
+  return {
+    useAuth: () => ({
+      signIn: jest.fn(),
+    }),
+  };
+});
 
-    debug();
+describe('Login Page', () => {
+  it('should be able to log in', async () => {
+    const { getByPlaceholderText, getByText } = render(<Login />);
+
+    const emailField = getByPlaceholderText('Email');
+    const passwordField = getByPlaceholderText('Password');
+    const buttonElement = getByText('Log in')
+
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
+    });
+
   });
 });
 
-// test
