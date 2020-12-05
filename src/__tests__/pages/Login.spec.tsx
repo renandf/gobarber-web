c3c8b67ec127c9552ef75686609ad792ce7040ee
Renan Castro <renandf@gmail.com>
2020-12-05 11:45:21 +1100

    finish tests for Login page

--- src/__tests__/pages/Login.spec.tsx
+++ src/__tests__/pages/Login.spec.tsx
@@ -4,6 +4,8 @@ import { render, fireEvent, waitFor } from '@testing-library/react';
 import Login from '../../pages/Login';
 
 const mockedHistoryPush = jest.fn();
+const mockedSignIn = jest.fn();
+const mockedAddToast = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
@@ -17,12 +19,26 @@ jest.mock('react-router-dom', () => {
 jest.mock('../../hooks/auth', () => {
   return {
     useAuth: () => ({
-      signIn: jest.fn(),
+      signIn: mockedSignIn,
+    }),
+  };
+});
+
+jest.mock('../../hooks/toast', () => {
+  return {
+    useToast: () => ({
+      addToast: mockedAddToast,
     }),
   };
 });
 
 describe('Login Page', () => {
+  beforeEach(() => {
+    mockedHistoryPush.mockClear();
+    mockedSignIn.mockClear();
+    mockedAddToast.mockClear();
+  });
+
   it('should be able to log in', async () => {
     const { getByPlaceholderText, getByText } = render(<Login />);
 
@@ -38,7 +54,56 @@ describe('Login Page', () => {
     await waitFor(() => {
       expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
     });
+  });
 
+  it('should not be able to log in with missing credentials', async () => {
+    const { getByPlaceholderText, getByText } = render(<Login />);
+
+    const emailField = getByPlaceholderText('Email');
+    const passwordField = getByPlaceholderText('Password');
+    const buttonElement = getByText('Log in')
+
+    fireEvent.change(emailField, { target: { value: '' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).not.toHaveBeenCalled();
+      expect(mockedAddToast).toHaveBeenCalledWith(
+        expect.objectContaining({
+          type: 'error',
+          title: 'Missing credentials',
+        }),
+      );
+    });
+  });
+
+  it('should not be able to log in with wrong credentials', async () => {
+    mockedSignIn.mockImplementation(() => {
+      throw new Error();
+    });
+
+    const { getByPlaceholderText, getByText } = render(<Login />);
+
+    const emailField = getByPlaceholderText('Email');
+    const passwordField = getByPlaceholderText('Password');
+    const buttonElement = getByText('Log in')
+
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).not.toHaveBeenCalled();
+      expect(mockedAddToast).toHaveBeenCalledWith(
+        expect.objectContaining({
+          type: 'error',
+          title: 'Authentication error',
+        }),
+      );
+    });
   });
 });
 
