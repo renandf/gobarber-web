c59310ea7e500b2b6dfeca908e7f768cd9c263e8
Renan Castro <renandf@gmail.com>
2020-12-07 07:16:20 +1100

    fix: add missing 'history' dependency on function

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -66,7 +66,7 @@ const Login: React.FC = () => {
         description: 'Please make sure your email and password are valid.'
       });
     }
-  }, [signIn, addToast]);
+  }, [signIn, addToast, history]);
 
   return (
     <Container>
